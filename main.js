f30bd4ebc8a2f2a20c1abf74115d1fcbdadaafb0
Adityacypto <84172029+Adityacypto@users.noreply.github.com>
2022-08-07 13:21:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function preload(){
+
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function setup(){
+
+    canvas = createCanvas(500,500);
+    canvas.center();
+    background("white");
+
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+
+}
+
+function clearCanvas(){
+
+    background("white");
+}
+
+function draw() {
+    
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed) {
+
+        line(pmouseX, pmouseY, mouseX, mouseY);
+         
+
+    }
+}
+
+function classifyCanvas() {
+
+    classifier.classify(canvas,gotResult);
+}
+
+
+function gotResult(error, results)  {
+
+    if(error){
+        console.log(error);
+    }
+    else {
+
+        console.log(results);
+
+        document.getElementById("label").innerHTML = "Label:" + results[0].label;
+        document.getElementById("confidence").innerHTML = "Confidence:" + Math.round(results[0].confidence *100) + '%';
+        utter_this = new SpeechSynthesisUtterance(results[0].label);
+        synth.speak(utter_this);
+    }
+}
